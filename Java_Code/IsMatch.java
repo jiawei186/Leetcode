0f7d6fa6ceeee19cfd7c45eb107a9d37d888fcc4
Degree <jiawei186@outlook.com>
2022-11-18 12:42:10 +0800

    IsMatch

---
+++ Java_Code/IsMatch.java
@@ -0,0 +1,43 @@
+// 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+// '.' 匹配任意单个字符
+// '*' 匹配零个或多个前面的那一个元素
+// 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+public class IsMatch {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        if (n == 0) {
+            return m == 0;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < n + 1; ++j) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        for (int i = 1; i < m + 1; ++i) {
+            for (int j = 1; j < n + 1; ++j) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String a = "aaa";
+        String b = "a*";
+        IsMatch c = new IsMatch();
+
+        System.out.println(c.isMatch(a, b));
+    }
+}
