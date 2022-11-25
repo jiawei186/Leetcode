8a3902d0b72058ba6eb2dfe74b52938516930cf6
Degree <jiawei186@outlook.com>
2022-11-25 09:19:48 +0800

    LetterCombinations

---
+++ Java_Code/LetterCombinations.java
@@ -0,0 +1,35 @@
+// 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinations {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits.length() == 0) {
+            return ans;
+        }
+        ans.add("");
+        String[] d = new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (char i : digits.toCharArray()) {
+            String s = d[i - '2'];
+            List<String> t = new ArrayList<>();
+            for (String a : ans) {
+                for (String b : s.split("")) {
+                    t.add(a + b);
+                }
+            }
+            ans = t;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String a = "45";
+        LetterCombinations b = new LetterCombinations();
+
+        System.out.println(b.letterCombinations(a));
+    }
+}
