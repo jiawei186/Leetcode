57427ed7dcf47c3413cf6c026a27ca9cb8c03f4e
degree <jiawei186@outlook.com>
2022-12-12 10:20:26 +0800

    a

---
+++ Java/ReverseKGroup.java
@@ -0,0 +1,65 @@
+//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+//
+//k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+//
+//你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+
+class ReverseKGroup {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {this.val = val;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy, cur = dummy;
+        while (cur.next != null) {
+            for (int i = 0; i < k && cur != null; ++i) {
+                cur = cur.next;
+            }
+            if (cur == null) {
+                return dummy.next;
+            }
+            ListNode t = cur.next;
+            cur.next = null;
+            ListNode start = pre.next;
+            pre.next = reverseList(start);
+            start.next = t;
+            pre = start;
+            cur = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, p = head;
+        while (p != null) {
+            ListNode q = p.next;
+            p.next = pre;
+            pre = p;
+            p = q;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ReverseKGroup.ListNode a = new ReverseKGroup.ListNode(3);
+
+        int i = 0;
+        while (i < 2) {
+            a = new ReverseKGroup.ListNode(2 - i, a); // [1, 2, 3]
+            i++;
+        }
+
+        ReverseKGroup d = new ReverseKGroup();
+        a = d.reverseKGroup(a, 2);
+
+        while (a != null) {
+            System.out.println(a.val);
+            a = a.next;
+        }
+    }
+}
