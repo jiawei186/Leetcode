f53854a350eec377119ec4c88ef1966334525979
degree <jiawei186@outlook.com>
2022-12-17 19:14:59 +0800

    a

---
+++ Java/StrStr.java
@@ -0,0 +1,40 @@
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+// 如果 needle 不是 haystack 的一部分，则返回  -1 。
+
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        if ("".equals(needle)) {
+            return 0;
+        }
+
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        int p = 0;
+        int q = 0;
+        while (p < len1) {
+            if (haystack.charAt(p) == needle.charAt(q)) {
+                if (len2 == 1) {
+                    return p;
+                }
+                ++p;
+                ++q;
+            } else {
+                p -= q - 1;
+                q = 0;
+            }
+
+            if (q == len2) {
+                return p - q;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String a = "leetcode";
+        String b = "etc";
+        StrStr c = new StrStr();
+
+        System.out.println(c.strStr(a, b));
+    }
+}
