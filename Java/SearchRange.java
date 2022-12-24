36f8c62960178ac1212918ab17ebabd7d147ff2a
degree <jiawei186@outlook.com>
2022-12-24 10:25:26 +0800

    a

---
+++ Java/SearchRange.java
@@ -0,0 +1,37 @@
+// 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+
+// 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+import java.util.Arrays;
+
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int l = search(nums, target);
+        int r = search(nums, target + 1);
+        return l == r ? new int[] {-1, -1} : new int[] {l, r - 1};
+    }
+
+    private int search(int[] nums, int x) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] >= x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] {1, 2, 2, 3};
+        int b = 2;
+
+        SearchRange c = new SearchRange();
+
+        System.out.println(Arrays.toString(c.searchRange(a, b)));
+    }
+}
