5a1d478a108d583b15ce61b4a6f6a942bb894acd
degree <jiawei186@outlook.com>
2022-12-25 10:26:31 +0800

    a

---
+++ Java/SearchInsert.java
@@ -0,0 +1,27 @@
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 请必须使用时间复杂度为 O(log n) 的算法。
+
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] {1, 3, 5, 6};
+        int b = 2;
+
+        SearchInsert c = new SearchInsert();
+
+        System.out.println(c.searchInsert(a, b));
+    }
+}
