2e88fb10b0a62013816163483ae8c36262efe4e5
degree <jiawei186@outlook.com>
2023-01-02 10:37:30 +0800

    a

---
+++ Java/FirstMissingPositive.java
@@ -0,0 +1,34 @@
+// 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+
+// 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (i + 1 != nums[i]) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] {3, 4, -1, 1};
+
+        FirstMissingPositive b = new FirstMissingPositive();
+
+        System.out.println(b.firstMissingPositive(a));
+    }
+}
