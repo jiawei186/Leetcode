cd5721403304df7e0e46242d976eeadbef3fe7ee
degree <jiawei186@outlook.com>
2023-01-09 12:59:35 +0800

    230109

---
+++ Java/PermuteUnique.java
@@ -0,0 +1,31 @@
+// 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+
+class PermuteUnique {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        int n = nums.length;
+        boolean[] used = new boolean[n];
+        Arrays.sort(nums);
+        dfs(0, n, nums, used, path, res);
+        return res;
+    }
+
+    private void dfs(
+        int u, int n, int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> res) {
+        if (u == n) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            dfs(u + 1, n, nums, used, path, res);
+            used[i] = false;
+            path.remove(path.size() - 1);
+        }
+    }
+}
